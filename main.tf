c929aa1017a2be9d0d3f005840694aedf5d1a05e
amiteng7 <amit.eng7@gmail.com>
2021-06-08 15:00:19 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,28 @@
+provider "google" {
+  credentials = file("service-account.json")
+
+  project = "cs-tmpl-test"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "vpc123test"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "test-amit"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network       = "default"
+    access_config {
+    }
+  }
+}
